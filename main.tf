5c9cf4364a8feaa337127d26622d7a5a71fcb3da
Logesh Shanmugavel <logesh81098@gmail.com>
2025-01-28 12:54:26 +0530

    Deployed IAM role and Policy for lambda function to create CollectionID in Rekognition service

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "s3" {
   source = "./module/S3"
 }
+
+module "iam-role" {
+  source = "./module/IAM-Role"
+}
---
+++ module/IAM-Role/main.tf
@@ -0,0 +1,70 @@
+#################################################################################################################################################
+#                                               Deploying IAM Role
+#################################################################################################################################################
+
+#IAM Role for lambda function to create AWS Rekognition collection ID
+
+resource "aws_iam_role" "rekognition-collection-id-role" {
+  name = "rekognition-collection-id-role"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+    }    
+    ]
+}  
+EOF
+}
+
+
+#################################################################################################################################################
+#                                               Deploying IAM Policy
+#################################################################################################################################################
+
+#IAM Policy for Lambda function to create AWS Rekognition collection ID
+
+resource "aws_iam_policy" "rekognition-collection-id-policy" {
+  name = "rekognition-collection-id-policy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+        "Sid": "CollectingRekognition",
+        "Effect": "Allow",
+        "Action": [
+            "rekognition:CreateCollection",
+            "rekognition:DeleteCollection",
+            "rekognition:ListCollections"
+        ],
+        "Resource": "*"
+    },
+    {
+        "Sid": "CloudWatchLogGroup",
+        "Effect": "Allow",
+        "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+        ],
+        "Resource": "arn:aws:logs:*:*:*"
+    }
+    ]
+}  
+EOF
+}
+
+#################################################################################################################################################
+#                                            IAM Role and Policy Attachment
+#################################################################################################################################################
+
+resource "aws_iam_role_policy_attachment" "rekognition-collection-id" {
+  role = aws_iam_role.rekognition-collection-id-role.id
+  policy_arn = aws_iam_policy.rekognition-collection-id-policy.arn
+}
